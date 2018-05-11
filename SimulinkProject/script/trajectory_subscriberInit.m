e6891e6b6ab94e5e393ba2be9e261f015cf57b65
Kroc <32593106+CheungKroc@users.noreply.github.com>
2018-05-11 20:16:58 +0800

    v0.6
    2018/5/11 删除一些无效的文件

--- SimulinkProject/script/trajectory_subscriberInit.m
+++
@@ -1,5 +0,0 @@
-%trajectory_subscriberInit
-%A script used to create a subscriber which is subscribed to a topic named
-%/trajectory_topic
-
-trajectory_receiver=rossubscriber('/trajectory_topic','trajectory_msgs/JointTrajectory',{@trajReceiverCallback,msg_armControllerGoal});
